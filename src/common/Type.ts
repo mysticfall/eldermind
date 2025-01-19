9ec3b57aae0393b130e463834acd2586aba55309
Xavier Cho <mysticfallband@gmail.com>
2025-01-19 16:42:00 +0900

    Add traverseRecord function to Type module
    Introduced a new `traverseRecord` function using `TR.Traversable.traverse` to complement the existing `traverseOption`. This enhances the module's capabilities for handling operations on records with applicative transformations.

--- src/common/Type.ts
+++ src/common/Type.ts
@@ -1,4 +1,5 @@
 import * as TA from "@effect/typeclass/data/Array"
+import * as TR from "@effect/typeclass/data/Record"
 import * as TFX from "@effect/typeclass/data/Effect"
 import * as TO from "@effect/typeclass/data/Option"
 
@@ -7,6 +8,8 @@ const applicative = TFX.getApplicative()
 export const traverseArray = TA.Traversable.traverse(applicative)
 
 export const traverseOption = TO.Traversable.traverse(applicative)
+
+export const traverseRecord = TR.Traversable.traverse(applicative)
 
 export type ConstructorType<T> = T extends new (...args: infer A) => unknown
     ? A
