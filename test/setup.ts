a9fac299812287258774771b60db412b157a835c
Xavier Cho <mysticfallband@gmail.com>
2025-03-31 23:55:55 +0900

    Add Rollup plugins and refactor Vite configuration
    Integrated new Rollup plugins for external globals and node resolve to optimize build processing. Updated Vite configuration for better environment-based builds, including debug mode handling, and added a test setup file for global runtime support. Dependencies and scripts updated accordingly in package.json.

---
+++ test/setup.ts
@@ -0,0 +1,3 @@
+import * as skyrimPlatform from "@skyrim-platform/skyrim-platform"
+// @ts-expect-error Required due to the way Skyrim Platform works in runtime.
+globalThis.skyrimPlatform = skyrimPlatform
--- vite.config.js
+++ vite.config.js
@@ -3,52 +3,75 @@ import path from "path"
 import tsconfigPaths from "vite-tsconfig-paths"
 import {globSync} from "glob"
 import {fileURLToPath} from "node:url"
+import nodeExternals from "rollup-plugin-node-externals"
+import externalGlobals from "rollup-plugin-external-globals"
+import nodeResolve from "@rollup/plugin-node-resolve"
 
-export default defineConfig({
-    plugins: [tsconfigPaths()],
-    build: {
-        rollupOptions: {
-            input: Object.fromEntries(
-                globSync("src/**/*.ts").map(file => [
-                    path.relative(
-                        "src",
-                        file.slice(0, file.length - path.extname(file).length)
-                    ),
-                    fileURLToPath(new URL(file, import.meta.url))
-                ])
-            ),
-            output: [
-                {
-                    entryFileNames: "[name].js",
-                    chunkFileNames: "[name]-[hash].js",
-                    format: "esm",
-                    dir: "dist/esm"
-                },
-                {
-                    entryFileNames: "[name].js",
-                    chunkFileNames: "[name]-[hash].js",
-                    format: "cjs",
-                    dir: "dist/cjs"
-                }
-            ],
-            external: ["path", "stream", "@skyrim-platform/skyrim-platform"],
-            preserveEntrySignatures: "allow-extension"
-        }
-    },
-    test: {
-        include: ["./test/**/*.test.ts"],
-        coverage: {
-            provider: "v8",
-            reporter: ["json-summary", "html"],
-            exclude: [
-                "docs",
-                "coverage",
-                "dist",
-                "test",
-                "**/index.ts",
-                "*.config.js",
-                "*.config.mjs"
-            ]
+export default defineConfig(({mode}) => {
+    const isDebug = mode === "development"
+
+    return {
+        plugins: [
+            tsconfigPaths(),
+            nodeExternals({
+                deps: false,
+                peerDeps: true
+            }),
+            externalGlobals({
+                skyrimPlatform: "skyrimPlatform"
+            })
+        ],
+        build: {
+            rollupOptions: {
+                input: Object.fromEntries(
+                    globSync("src/**/*.ts").map(file => [
+                        path.relative(
+                            "src",
+                            file.slice(
+                                0,
+                                file.length - path.extname(file).length
+                            )
+                        ),
+                        fileURLToPath(new URL(file, import.meta.url))
+                    ])
+                ),
+                output: [
+                    {
+                        entryFileNames: "[name].js",
+                        chunkFileNames: "[name]-[hash].js",
+                        format: "esm",
+                        dir: "dist/esm"
+                    },
+                    {
+                        entryFileNames: "[name].js",
+                        chunkFileNames: "[name]-[hash].js",
+                        format: "cjs",
+                        dir: "dist/cjs"
+                    }
+                ],
+                preserveEntrySignatures: "allow-extension",
+                plugins: [nodeResolve()]
+            },
+            minify: !isDebug,
+            sourcemap: isDebug,
+            target: "esnext"
+        },
+        test: {
+            include: ["./test/**/*.test.ts"],
+            setupFiles: "./test/setup.ts",
+            coverage: {
+                provider: "v8",
+                reporter: ["json-summary", "html"],
+                exclude: [
+                    "docs",
+                    "coverage",
+                    "dist",
+                    "test",
+                    "**/index.ts",
+                    "*.config.js",
+                    "*.config.mjs"
+                ]
+            }
         }
     }
 })
